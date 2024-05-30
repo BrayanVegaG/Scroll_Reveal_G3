2fb91e64b51b9e0d6211ba8aa7e7777026fb9e84
Brayan Vega <93897118+BrayanVegaG@users.noreply.github.com>
2024-05-30 15:35:42 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+/*!
+* Start Bootstrap - Scrolling Nav v5.0.6 (https://startbootstrap.com/template/scrolling-nav)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    
+   
+
+});
